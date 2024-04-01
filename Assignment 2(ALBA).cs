c374a3a4cf5c016435ec8f29b1ecc7d7e227e72f
Danica <165607381+DanicaGrace21@users.noreply.github.com>
2024-04-01 11:44:05 +0800

    Add files via upload

---
+++ Assignment 2(ALBA).cs
@@ -0,0 +1,24 @@
+﻿namespace Assignment_2_ALBA_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int A = 1; A < 101; A++)
+            {
+                if (A % 15 == 0){
+                    Console.WriteLine("Hello Good Bye");                   
+                }
+                else if (A % 5 == 0){
+                    Console.WriteLine("Good Bye");
+                }
+                else if (A % 3 == 0){
+                    Console.WriteLine("Hello");
+                }
+                else{
+                    Console.WriteLine(A);
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Alba_DG_Assignment_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int A = 1; A < 101; A++)
+            {
+                if (A % 15 == 0)
+                {
+                    Console.WriteLine("Hello Good Bye");
+                }
+                else if (A % 5 == 0)
+                {
+                    Console.WriteLine("Good Bye");
+                }
+                else if (A % 3 == 0)
+                {
+                    Console.WriteLine("Hello");
+                }
+                else
+                {
+                    Console.WriteLine(A);
+                }
+            }
+        }
+    }
+}
